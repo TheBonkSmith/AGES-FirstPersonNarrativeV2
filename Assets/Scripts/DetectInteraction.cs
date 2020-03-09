8c619694dc43aa87ffd707653d814a56e4d32ad9
TheBonkSmith <54965620+TheBonkSmith@users.noreply.github.com>
2020-03-09 10:50:26 -0500

    Fixes and First three videos of Milestone 3
    there is an issue with the Interact display text but i'll fix that soon.

--- Assets/Scripts/DetectInteraction.cs
+++ Assets/Scripts/DetectInteraction.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,24 +13,52 @@ public class DetectInteraction : MonoBehaviour
     [SerializeField]
     private float maxRange = 5.0f;
 
-
+    ///Summary
+    /// Event raised when player looks at a different IInteractive
+    ///Summary
+    public static event Action<IInteractive> LookedAtInteractiveChanged;
     
-    void Start()
+    public IInteractive LookedAtInteractive
     {
-        
+        get { return lookedAtInteractive; }
+        private set 
+        {
+            bool isInteractiveChanged = value != lookedAtInteractive;
+            if (isInteractiveChanged)
+            {
+                lookedAtInteractive = value;
+                LookedAtInteractiveChanged?.Invoke(lookedAtInteractive);
+            }
+        }
     }
 
+    private IInteractive lookedAtInteractive;
 
     private void FixedUpdate()
+    {
+        lookedAtInteractive = GetLookedAtInteractive();
+    }
+
+    /// <summary>
+    /// Raycasts forward from the camera to look to IInteractives
+    /// </summary>
+    /// <returns>The first Interactive Detected, or null</returns>
+    private IInteractive GetLookedAtInteractive()
     {
         Debug.DrawRay(rayCastOrigin.position, rayCastOrigin.forward * maxRange, Color.red);
         RaycastHit hitInfo;
         bool objectWasDetected = Physics.Raycast(rayCastOrigin.position, rayCastOrigin.forward, out hitInfo, maxRange);
 
-        if(objectWasDetected)
+        IInteractive interactive = null;
+
+        lookedAtInteractive = interactive;
+
+        if (objectWasDetected)
         {
-            Debug.Log($"Player is looking at: { hitInfo.collider.gameObject.name}");
+            //Debug.Log($"Player is looking at: { hitInfo.collider.gameObject.name}");
+            interactive = hitInfo.collider.gameObject.GetComponent<IInteractive>();
         }
-        
+
+        return interactive;
     }
 }
---
+++ Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour, IInteractive
+{
+    [SerializeField]
+    private string displayText = nameof(InteractiveObject);
+    public string DisplayText => displayText;
+
+    public void InteractWith()
+    {
+        Debug.Log($"Player just interacted with {gameObject.name}.");
+    }
+}
