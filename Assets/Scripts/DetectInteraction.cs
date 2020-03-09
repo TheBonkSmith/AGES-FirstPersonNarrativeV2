08cb3660bc33ba3f5cf717bbe9cf2d90c5ba7893
TheBonkSmith <54965620+TheBonkSmith@users.noreply.github.com>
2020-03-09 00:14:26 -0500

    doing the stuff on the videos
    vagueness is vague Raycasting videos

---
+++ Assets/Scripts/DetectInteraction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectInteraction : MonoBehaviour
+{
+    [Tooltip("Starting point of raycast used to detect interaction")]
+    [SerializeField]
+    private Transform rayCastOrigin;
+
+    [Tooltip("Search range for interaction")]
+    [SerializeField]
+    private float maxRange = 5.0f;
+
+
+    
+    void Start()
+    {
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        Debug.DrawRay(rayCastOrigin.position, rayCastOrigin.forward * maxRange, Color.red);
+        RaycastHit hitInfo;
+        bool objectWasDetected = Physics.Raycast(rayCastOrigin.position, rayCastOrigin.forward, out hitInfo, maxRange);
+
+        if(objectWasDetected)
+        {
+            Debug.Log($"Player is looking at: { hitInfo.collider.gameObject.name}");
+        }
+        
+    }
+}
