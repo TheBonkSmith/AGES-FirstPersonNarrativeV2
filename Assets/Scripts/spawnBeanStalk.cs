e3565d772dd81251dfe39d4b26a39a0e4856fb14
TheBonkSmith <54965620+TheBonkSmith@users.noreply.github.com>
2020-05-08 20:21:45 -0500

    milestone 5 stuff?
    IT's giving me weird vibes I hope this works

---
+++ Assets/Scripts/spawnBeanStalk.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnBeanStalk : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject beanStalkPrefab;
+
+    [Tooltip("Starting point of raycast used to detect interaction")]
+    [SerializeField]
+    private Transform rayCastOrigin;
+    RaycastHit hit;
+
+    public Collider coll;
+
+    //private Animator animator;
+
+    //[SerializeField]
+    //protected string displayText = nameof(InteractiveObject);
+    //public virtual string DisplayText => displayText;
+
+    void Awake()
+    {
+        //animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        coll = GetComponent<Collider>();
+    }
+    void Update()
+    {
+
+        RaycastHit hit = GetComponent<DetectInteraction>().hitInfo;
+
+
+        spawnThatStalk(hit.point);
+    }
+
+    //public virtual void InteractWith()
+    //{
+    //    try
+    //    {
+    //        //audioSource.Play();
+    //    }
+    //    catch (System.Exception)
+    //    {
+
+    //        throw new System.Exception("Interactive Object requires an Auidio Source Component.");
+    //    }
+
+    //    Debug.Log($"Player just interacted with {gameObject.name}.");
+    //    Instantiate(beanStalkPrefab, gameObject.transform.position, Quaternion.identity);
+    //}
+    void spawnThatStalk(Vector3 instantiationPoint)
+    {
+        if (Input.GetButtonDown("SpawnStalk"))
+        {
+
+         
+           Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+           RaycastHit hit;
+
+          // if (coll.Raycast(ray, out hit, 100.0f))
+           //{
+                GameObject bean = Instantiate(beanStalkPrefab, instantiationPoint, Quaternion.identity);
+                //transform.position = ray.GetPoint(100.0f);
+          // }
+          
+
+
+
+
+
+
+            //Debug.Log(hit.point + "Hits a million" + "  " + instantiationPoint + " der");
+
+            //GameObject bean = Instantiate(beanStalkPrefab, instantiationPoint, Quaternion.identity);
+
+            //Debug.Log("Hey boi? WHere you at?" + bean.transform.position);
+        }
+
+    }
+}
